d3ed164d8e4d2e271654acff777de1364db47d5b
Wahyu NT <wahyuntx28@gmail.com>
2024-12-22 20:32:42 +0700

    add CRUD operations for skills management with MongoDB integration

--- index.py
+++ index.py
@@ -1,34 +1,41 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb+srv://root:root@cluster0.lhlezvs.mongodb.net/web_wahyunt?retryWrites=true&w=majority')
+db = client['web_wahyunt']
+skills_collection  = db['skills']
 
-@app.route('/')
-def home():
-    return 'Home Page Route - nice work Andrew!!!'
+@app.route('/skills', methods=['GET'])
+def get_skills():
+    try:
+        skills = list(skills_collection.find({}, {'_id': 0}))  # Exclude _id
+        return jsonify(skills)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
+# POST: Tambah skill baru
+@app.route('/skills', methods=['POST'])
+def add_skill():
+    data = request.json
+    new_skill = {'name': data['name'], 'level': data['level']}
+    skills_collection.insert_one(new_skill)
+    return jsonify(new_skill), 201
 
-@app.route('/about')
-def about():
-    return 'About Page Route'
+# PUT: Update skill berdasarkan nama
+@app.route('/skills/<string:skill_name>', methods=['PUT'])
+def update_skill(skill_name):
+    data = request.json
+    skills_collection.update_one({'name': skill_name}, {'$set': data})
+    updated_skill = skills_collection.find_one({'name': skill_name}, {'_id': 0})
+    return jsonify(updated_skill)
 
+# DELETE: Hapus skill berdasarkan nama
+@app.route('/skills/<string:skill_name>', methods=['DELETE'])
+def delete_skill(skill_name):
+    skills_collection.delete_one({'name': skill_name})
+    return '', 204
 
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
-
-
-@app.route('/api')
-def api():
-    with open('data.json', mode='r') as my_file:
-        text = my_file.read()
-        return text
-    
-
-    
+if __name__ == '__main__':
+    app.run(debug=True)
