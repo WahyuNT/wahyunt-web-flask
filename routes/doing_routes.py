084024f51700316ac1631b4b79977a5a571f30ea
Wahyu NT <wahyuntx28@gmail.com>
2024-12-24 15:53:18 +0700

    fix: update URL prefix for doing routes to '/api/doings'

--- routes/doing_routes.py
+++ routes/doing_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from controllers.DoingController import get_doing
 
-doing_bp = Blueprint('doing', __name__, url_prefix='/api/doing')
+doing_bp = Blueprint('doing', __name__, url_prefix='/api/doings')
 
 doing_bp.route('/',methods=['GET'])(get_doing)
