10cfc0bc5014756e85553e88ffe94aaa4d894d60
Wahyu NT <wahyuntx28@gmail.com>
2024-12-26 15:01:07 +0700

    refactor: clean up whitespace and improve error handling in get_portofolio function

--- controllers/PortofolioController.py
+++ controllers/PortofolioController.py
@@ -11,18 +11,18 @@ portofolio_image_collection = db['imageportofolios']
 
 
 def get_portofolio():
-    keyword = request.args.get('keyword', '')
+    keyword = request.args.get('keyword', '') 
     try:
         if keyword:
             portfolio = list(portofolio_collection.find(
-                {"type": {"$regex": keyword, "$options": "i"}},
+                {"type": {"$regex": keyword, "$options": "i"}},  # Regex untuk pencarian
                 {"_id": 0}
             ))
         else:
             portfolio = list(portofolio_collection.find({}, {"_id": 0}))
-        return jsonify({"data": portfolio})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"data":portfolio})
+    except Exception as e :
+        return jsonify({"error" : str(e)}),500
     
     
 def get_portofolio_detail(slug):
